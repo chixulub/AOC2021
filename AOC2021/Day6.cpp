99c8b96ba72c1b157b1bb03c387b85cd2b008637
Sven Mattias Gunnarsson <smgunnarsson@gmail.com>
2021-12-06 20:37:46 +0100

    Optimize day 6

--- AOC2021/Day6.cpp
+++ AOC2021/Day6.cpp
@@ -7,48 +7,34 @@ static auto stoi_transform = [](string const& s) { return stoi(s); };
 static auto day = setDay(6, "",
 	[&](auto answer)
 	{
-		auto initial = split(lines_in_file("input/6.txt").front(), ',', stoi_transform);
+		vector<uint64_t> fishes(264, 0);
 
-		map<uint64_t, uint64_t> fishes{ {0,0}, {1,0}, {2,0}, {3,0}, {4,0}, {5,0}, {6,0}, {7,0} };
-
-		for (uint64_t i : initial)
+		for (uint64_t i : split(lines_in_file("input/6.txt").front(), ',', stoi_transform))
 		{
-			fishes[i] += 1;
+			++fishes[i];
 		}
 
-		map<uint64_t, uint64_t> day80;
-		for (int i = 0; i < 256; ++i)
-		{
-			map<uint64_t, uint64_t> next;
-
-			next[0] = fishes[1];
-			next[1] = fishes[2];
-			next[2] = fishes[3];
-			next[3] = fishes[4];
-			next[4] = fishes[5];
-			next[5] = fishes[6];
-			next[6] = fishes[7] + fishes[0];
-			next[7] = fishes[8];
-			next[8] = fishes[0];
-
-			swap(fishes, next);
-
-			if (i == 79)
-			{
-				day80 = fishes;
-			}
-		}
+		uint64_t *f = &fishes[0], *end = &fishes[256], *sample = &fishes[81];
 
 		uint64_t part1 = 0;
-		for (auto& it : day80)
+		for (; f < end ; ++f)
 		{
-			part1 += it.second;
+			if (f == sample)
+			{
+				for (int j = 0; j < 8; ++j)
+				{
+					part1 += f[j];
+				}
+			}
+
+			f[7] += f[0];
+			f[9]  = f[0];
 		}
 
 		uint64_t part2 = 0;
-		for (auto& it : fishes)
+		for (int j = 0; j < 9; ++j)
 		{
-			part2 += it.second;
+			part2 += f[j];
 		}
 
 		answer(1, part1);
